970a51926d6a7f68eedc03b345be1a8fc5ff3812
diyor <diyor@gmail.com>
2021-11-22 18:54:32 +0500

    js

---
+++ script.js
@@ -0,0 +1,64 @@
+
+var str = prompt('Введите своё имя');
+
+console.log(str);
+
+alert(str);
+
+
+var name = prompt('Введите свой возраст');
+
+console.log(name);
+
+alert(name);
+
+
+var plus = prompt('7 + 7')
+var otvetPlus = 7 + 7;
+console.log('Ваш ответ: ' + plus + ' правильный ответ: ' + otvetPlus);
+
+alert(plus);
+
+
+var minus = prompt('7 - 5')
+var otvetMinus = 7 - 5;
+console.log('Ваш ответ: ' + minus + ' правильный ответ: ' + otvetMinus);
+
+alert(minus);
+
+
+var multiplication = prompt('7 * 7')
+
+var otvetmultiplication = 7 * 7;
+console.log('Ваш ответ: ' + multiplication + ' правильный ответ: ' + otvetmultiplication);
+
+alert(multiplication);
+
+
+var division = prompt('4 / 2')
+var otvetdivision = 4 / 2;
+console.log('Ваш ответ: ' + division + ' правильный ответ: ' + otvetdivision);
+
+alert(division);
+
+
+var remainder = prompt('6 % 5')
+var otvetremainder = 6 % 5;
+console.log('Ваш ответ: ' + remainder + ' правильный ответ: ' + otvetremainder);
+
+alert(remainder);
+
+
+alert('check your answers in developer console')
+
+
+
+
+
+
+
+
+
+
+
+
